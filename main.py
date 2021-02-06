0fac11268f5f73529a16df77796371eb0b925eea
PSDat123 <sieudat123@gmail.com>
2021-02-06 16:20:29 +0700

    smol update

--- main.py
+++ main.py
@@ -6,18 +6,71 @@ import gc
 import sys
 import curses
 import time
+from ctypes import POINTER, WinDLL, Structure, sizeof, byref
+from ctypes.wintypes import BOOL, SHORT, WCHAR, UINT, ULONG, DWORD, HANDLE
+
+
+LF_FACESIZE = 32
+STD_OUTPUT_HANDLE = -11
+
+
+class COORD(Structure):
+    _fields_ = [
+        ("X", SHORT),
+        ("Y", SHORT),
+    ]
+
+
+class CONSOLE_FONT_INFOEX(Structure):
+    _fields_ = [
+        ("cbSize", ULONG),
+        ("nFont", DWORD),
+        ("dwFontSize", COORD),
+        ("FontFamily", UINT),
+        ("FontWeight", UINT),
+        ("FaceName", WCHAR * LF_FACESIZE)
+    ]
+
+
+kernel32_dll = WinDLL("kernel32.dll")
+
+get_last_error_func = kernel32_dll.GetLastError
+get_last_error_func.argtypes = []
+get_last_error_func.restype = DWORD
+
+get_std_handle_func = kernel32_dll.GetStdHandle
+get_std_handle_func.argtypes = [DWORD]
+get_std_handle_func.restype = HANDLE
+
+get_current_console_font_ex_func = kernel32_dll.GetCurrentConsoleFontEx
+get_current_console_font_ex_func.argtypes = [HANDLE, BOOL, POINTER(CONSOLE_FONT_INFOEX)]
+get_current_console_font_ex_func.restype = BOOL
+
+set_current_console_font_ex_func = kernel32_dll.SetCurrentConsoleFontEx
+set_current_console_font_ex_func.argtypes = [HANDLE, BOOL, POINTER(CONSOLE_FONT_INFOEX)]
+set_current_console_font_ex_func.restype = BOOL
+
+stdout = get_std_handle_func(STD_OUTPUT_HANDLE)
+# Get current font characteristics
+font = CONSOLE_FONT_INFOEX()
+font.cbSize = sizeof(CONSOLE_FONT_INFOEX)
+res = get_current_console_font_ex_func(stdout, False, byref(font))
+font.dwFontSize.X = 10  # Changing X has no effect (at least on my machine)
+font.dwFontSize.Y = 11
+# Apply changes
+res = set_current_console_font_ex_func(stdout, False, byref(font))
+res = get_current_console_font_ex_func(stdout, False, byref(font))
 
 
 cap = cv2.VideoCapture('Bad Apple_144p.mp4')
 frameCount = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 frameWidth = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 frameHeight = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-print(frameHeight)
-os.system(f'mode con cols={frameWidth + 1} lines={frameHeight + 1}')
+
+cmd = f'mode {frameWidth + 1}, {frameHeight + 1}'
+os.system(cmd)
 
 buf = np.empty((frameCount, frameHeight, frameWidth, 3), np.dtype('uint8'))
-
-
 fc = 0
 ret = True
 out = ''
